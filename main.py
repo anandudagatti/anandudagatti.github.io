27412fb5323d5b805a22e367c81f4ffc6dc8603e
anandudagatti <anand.udagatti@ltts.com>
2019-07-11 16:24:15 +0530

    Revert "Update main.py"
    This reverts commit 5d0713eef668951bd2296737ea2d62da80ecd003.

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import git
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('./anandudagatti/MS_Constructions')
-        origin = repo.remotes.origin
-        repo.create_head('master', 
-    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-        origin.pull()
-        return '', 200
-    else:
-        return '', 400
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('./anandudagatti/MS_Constructions')
+            origin = repo.remotes.origin
+            repo.create_head('master', 
+        origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+            origin.pull()
+            return '', 200
+        else:
+            return '', 400
 
 @app.route("/")
 def home():
